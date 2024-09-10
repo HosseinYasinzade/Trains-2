e7f8218a271ff13794df6dbc64f6f452389c6f1b
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-09-10 08:54:03 +0330

    add score program

--- script.js
+++ script.js
@@ -21,3 +21,15 @@
 // if (userAge >= 18) console.log("You are an adult");
 // else if (userAge >= 13 && userAge < 18) console.log("You are a teenager");
 // else console.log("You are a child");
+
+// //////////////// score ///////////////////////////////
+
+const score = Number(prompt("enter number : "));
+
+if (score >= 90 && score <= 100) console.log("A+");
+else if (score >= 80 && score < 90) console.log("A");
+else if (score >= 70 && score < 80) console.log("B+");
+else if (score >= 60 && score < 70) console.log("B");
+else if (score >= 50 && score < 60) console.log("C");
+else if (score > 0 && score < 50) console.log("D");
+else console.log("Not avalable");
