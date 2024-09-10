9465801f2429af9870d30ba380eea325359f6e65
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-09-10 08:41:10 +0330

    add even or odd program

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // /////////////// Trafic Light //////////////////////////////////////////
 
-const traficLightColor = prompt("pleasse enter color: (red green yellow)");
+// const traficLightColor = prompt("pleasse enter color: (red green yellow)");
 
-if (traficLightColor === "red") console.log("stop");
-else if (traficLightColor === "yellow") console.log("Slow down");
-else if (traficLightColor === "green") console.log("go");
-else console.log("Invalid color");
+// if (traficLightColor === "red") console.log("stop");
+// else if (traficLightColor === "yellow") console.log("Slow down");
+// else if (traficLightColor === "green") console.log("go");
+// else console.log("Invalid color");
 
 // //////////////////// odd / even ///////////////////////////////////
+
+// const userNumber = +prompt("Please enter number : ");
+
+// if (userNumber % 2 === 0) console.log(`The number ${userNumber} is even`);
+// else console.log(`The number ${userNumber} is odd`);
+
+// //////////////// Age Verification System /////////////////////////////
