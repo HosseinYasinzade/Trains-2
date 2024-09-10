633d7422092d6f9c1efe9d695560f458e4b1cb54
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-09-10 08:31:14 +0330

    add trafic light program

---
+++ script.js
@@ -0,0 +1,10 @@
+// /////////////// Trafic Light //////////////////////////////////////////
+
+const traficLightColor = prompt("pleasse enter color: (red green yellow)");
+
+if (traficLightColor === "red") console.log("stop");
+else if (traficLightColor === "yellow") console.log("Slow down");
+else if (traficLightColor === "green") console.log("go");
+else console.log("Invalid color");
+
+// //////////////////// odd / even ///////////////////////////////////
